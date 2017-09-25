6ba476c7ce7776d66213ba86465db950102a2e4c
Jaehong Kim <jaehong1007@gmail.com>
2017-09-25 17:08:14 +0900

    post_list는 published_Date가 존재하는 Post objects만 리턴, 각 post의 content에는 truncatewords:10필터 적용

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -4,10 +4,11 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    # post_list view가 published_date가 존재하는 Post목록만 보여주도록 수정
+
+    posts = Post.objects.filter(published_Date__isnull=False)
     context = {
         # posts key의 value는 QuerySet
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
-
