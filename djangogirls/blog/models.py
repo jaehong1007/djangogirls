1e390b37dd8d3bf9ecde41af1c5908fafe174b8b
Jaehong Kim <jaehong1007@gmail.com>
2017-09-26 15:08:24 +0900

    publish 함수 추가

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.db import models
 from django.conf import settings
 
@@ -12,3 +14,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+        """
+        게시글을 발행상태로 만듬
+            자신의 published_date를 timezone.now()로 할당
+            이후 self.save()를 호출
+        :return: 
+        """
+
+    def hide(self):
+        self.published_date = None
+        self.save()
+        """
+        게시글을 미발행상태로 만듬
+            자신의 published_date를 None으로 할당
+            이후 self.save()를 호출
+        :return:
+        """
